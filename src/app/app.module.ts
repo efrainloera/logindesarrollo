e41f58f6bca1cb65fd29f5b3d4b94a8312cfbc5e
efrain <efrainloerub14@gmail.com>
2022-09-28 19:14:09 -0500

    Añadio:funciones de login asi como tambien el diseño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+// Import the module from the SDK
+import { AuthModule } from '@auth0/auth0-angular';
+//Cuenta Auth0
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-hx9cy1se.us.auth0.com',
+      clientId: 'IsxyyM1rZo7V3siEjPlzWd1Vvi0Q90Z8'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
